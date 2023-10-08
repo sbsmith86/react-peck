c22777a5c3b5c7252990b32f76ff026081eb117b
Shae Smith <sbmith86@gmailcom>
2023-10-07 23:52:48 -0400

    add some elements to the layout

--- assets/js/Main.js
+++ assets/js/Main.js
@@ -3,11 +3,22 @@ import React, { useEffect, useState } from "react";
 export default function Main(props) {
   return (
     <div>
-      {/* Some sort of header - don't over think it.  */}
-      <header class="sticky top-0 bg-white shadow p-4"><code class="bg-gray-100">Peck Assignment</code></header>
-      <main class="p-4">
-        <p class="mb-6">What is Lorem Ipsum? Lorem Ipsum is simply dummy text of the printing and typesetting industry. Lorem Ipsum has been the industry's standard dummy text ever since the 1500s, when an unknown printer took a galley of type and scrambled it to make a type specimen book. It has survived not only five centuries, but also the leap into electronic typesetting, remaining essentially unchanged. It was popularised in the 1960s with the release of Letraset sheets containing Lorem Ipsum passages, and more recently with desktop publishing software like Aldus PageMaker including versions of Lorem Ipsum.</p>
-      </main>
+      <div className="flex justify-center">
+        <p className="my-12">Enter your zip code to find a food truck near you</p>
+      </div>
+      <div className="flex justify-center py-4">
+        <form className="w-full max-w-sm">
+          <div className="flex items-center border-b py-2">
+            <input className="appearance-none bg-transparent border-none w-full text-gray-700 mr-3 py-1 px-2 leading-tight focus:outline-none" type="text" placeholder="Zip Code" aria-label="Zip Code" />
+            <button className="flex-shrink-0 bg-gray-500 hover:bg-gray-700 border-gray-500 hover:border-gray-700 text-sm border-4 text-white py-1 px-2 rounded" type="button">
+              Search
+            </button>
+          </div>
+        </form>
+      </div>
+      <div className="bg-blue-100">
+        <p className="my-12">map here</p>
+      </div>
     </div>
   );
 }
