fb6b36d5b3d5e1abe0acf2000a89712b5845513f
Shae Smith <sbmith86@gmailcom>
2023-10-08 14:44:26 -0400

    add form handling

--- assets/js/Main.js
+++ assets/js/Main.js
@@ -3,14 +3,20 @@ import MapContainer from "./MapContainer";
 import SearchForm from "./SearchForm";
 
 export default function Main(props) {
+  // const [zipCode, setZipCode] = useState("");
+
+  const handleSearch = (zipCodeVal) => {
+    console.log(zipCodeVal);
+  }
+
   return (
     <div>
       <div className="flex justify-center my-8">
         {/* @TODO - Header Component */}
-        <h3 class="text-3xl font-bold">Enter your zip code to find a food truck near you</h3>
+        <h3 className="text-3xl font-bold">Enter your zip code to find a food truck near you</h3>
       </div>
       <div className="flex justify-center py-4">
-        <SearchForm />
+        <SearchForm handleSearch={handleSearch} />
       </div>
       <div className="bg-blue-100">
         <MapContainer />
--- assets/js/SearchForm.js
+++ assets/js/SearchForm.js
@@ -1,16 +1,36 @@
-import React from 'react';
+import React, { useState } from 'react';
 
-const SearchForm = () => {
-    // Key should be in env variable
-    return (
-        <form className="w-full max-w-sm">
-          <div className="flex items-center border-b py-2">
-            <input className="appearance-none bg-transparent border-none w-full text-gray-700 mr-3 py-1 px-2 leading-tight focus:outline-none" type="text" placeholder="Zip Code" aria-label="Zip Code" />
-            <button className="flex-shrink-0 bg-gray-500 hover:bg-gray-700 border-gray-500 hover:border-gray-700 text-sm border-4 text-white py-1 px-2 rounded" type="button">Search
-            </button>
-          </div>
-        </form>
-    );
+const SearchForm = ({handleSearch}) => {
+  const [formData, setFormData] = useState({
+    zipCode: "",
+  });
+
+  const [showValidationError, setShowValidationError] = useState(false);
+
+  const isUSAZipCode = (str) => {
+    return /^\d{5}(-\d{4})?$/.test(str);
+  }
+
+  const handleSubmit = (e) => {
+    e.preventDefault();
+
+    if (isUSAZipCode(formData.zipCode)) {
+      handleSearch(formData.zipCode);
+      setShowValidationError(false);
+    } else {
+      setShowValidationError(true);
+    }
+  }
+
+  return (
+    <form className="w-full max-w-sm" onSubmit={handleSubmit}>
+      <div className="flex items-center border-b py-2">
+        <input className="appearance-none bg-transparent border-none w-full text-gray-700 mr-3 py-1 px-2 leading-tight focus:outline-none" value={formData.zipCode} onChange={(e) => setFormData({...formData, zipCode: e.target.value})} type="text" name="zipcode" id="zipcode" />
+        <input type="submit" className="flex-shrink-0 bg-gray-500 hover:bg-gray-700 border-gray-500 hover:border-gray-700 text-sm border-4 text-white py-1 px-2 rounded" value="Submit" />
+      </div>
+      {showValidationError && (<p className='text-red-500'>Please enter a valid zip code.</p>) }
+    </form>
+  )
 }
 
 export default SearchForm;
